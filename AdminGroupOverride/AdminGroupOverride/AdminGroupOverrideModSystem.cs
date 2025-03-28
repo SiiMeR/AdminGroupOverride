68b10b678c8c45ef131534db3f276f337bced3b2
Siim Raudsepp <siimraudsepp@msn.com>
2025-03-28 20:08:18 +0200

    Initial release

---
+++ AdminGroupOverride/AdminGroupOverride/AdminGroupOverrideModSystem.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using Vintagestory.API.Server;
+using Vintagestory.API.Common;
+using Vintagestory.Server;
+
+namespace AdminGroupOverride;
+
+public class AdminGroupOverrideModSystem : ModSystem
+{
+    public override bool ShouldLoad(EnumAppSide forSide)
+    {
+        return forSide == EnumAppSide.Server;
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        var harmony = new Harmony(Mod.Info.ModID);
+
+        var original = AccessTools.Method(typeof(ServerySystemPlayerGroups), nameof(ServerySystemPlayerGroups.HasPlayerPrivilege));
+        var patch = AccessTools.Method(typeof(HasPlayerPrivilegePatch), nameof(HasPlayerPrivilegePatch.Prefix));
+
+        harmony.Patch(original, new HarmonyMethod(patch));
+    }
+
+}
---
+++ AdminGroupOverride/AdminGroupOverride/HasPlayerPrivilegePatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Vintagestory.API.Server;
+using Vintagestory.Server;
+
+namespace AdminGroupOverride;
+
+public class HasPlayerPrivilegePatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(ServerySystemPlayerGroups), nameof(ServerySystemPlayerGroups.HasPlayerPrivilege))]
+    public static bool Prefix(ref bool __result, IServerPlayer player)
+    {
+        if (player.Role.Code == "admin")
+        {
+            __result = true;
+            return false;
+        }
+        
+        return false;
+    }
+}
